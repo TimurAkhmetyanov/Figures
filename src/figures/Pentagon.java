3e2f08275bd2f05071dac8e1a2289ecf143fc54d
TimurPJ <timurchik0501@gmail.com>
2022-12-03 17:19:53 +0500

    Pentagon commit

--- src/figures/Pentagon.java
+++ src/figures/Pentagon.java
@@ -1,4 +1,73 @@
 package figures;
 
-public class Pentagon {
+public class Pentagon extends Figures {
+    private int a, b, c, d, e;
+
+    public Pentagon(int a, int b, int c, int d, int e) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    @Override
+    public double getS() {
+        return super.getS();
+    }
+
+    @Override
+    public void setS(double s) {
+        super.setS(s);
+    }
+
+    @Override
+    public double getP() {
+        return super.getP();
+    }
+
+    @Override
+    public void setP(double p) {
+        super.setP(p);
+    }
 }
